4d3eb755fc3dcff856acbbcb1f89726208c5d82f
PeterWright <peterwrighten@gmail.com>
2021-11-22 00:39:59 +0900

    アップデート：関数の順序を調整；基本関数を打ち込んだ。

--- httpd.c
+++ httpd.c
@@ -17,33 +17,156 @@
 #include<sys/wait.h>
 #include<stdlib.h>
 
-#define ISspace(x) isspace((int)(x));
+
+#define isSpace(x) isspace((int)(x));
 //#include<ctype.h>に定義される関数。
 //' ', '\r', '\t', '\n', '\v', '\f'（空白）であれば０以外の値を返し、そうではないと０を返す。
 #define SERVER_STRING "Server: jdbhttpd/0.1.0\r\n"
 
+int get_line(int, char *, int);
+void not_found(int);
+void headers(int, const char *);
+void server_file(int, const char *);
+void execute_cgi(int, const char *, const char *, const char *);
+void unimplemented(int);
 void accept_request(int);
 void bad_request(int);
 void cat(int, FILE *);
 void cannot_execute(int);
 void error_die(const char *);
-void execute_cgi(int, const char *, const char *, const char *);
-int get_line(int, char *, int);
-void headers(int, const char *);
-void not_found(int);
-void server_file(int, const char *);
 int startup(u_short *);
-void unimplemented(int);
+
 
 /**************************************************
  * 
  * 
  * 
+ * 
  * ************************************************/
+
+int get_line(int sock, char *buf, int size)
+{
+    int i = 0;
+    char c = '\0';
+    int n;
+
+    while((i < size - 1) && (c != '\n'))
+    {
+        n = recv(sock, &c, 1, 0);//recv()関数は<sys/socket.h>に定義される。
+        if(n > 0)
+        {
+            if(c == '\r')
+            {
+                n = recv(sock, &c, 1, MSG_PEEK);
+                if((n > 0) && (c == '\n'))
+                    recv(sock, &c, 1, 0);
+                else
+                    c = '\n';
+            }
+            buf[i] = c;
+            i++;
+        }
+        else
+            c = '\n';
+    
+    }
+    buf[i] = '\0';
+
+    return(i);
+}
+
+/**************************************************
+ * 
+ * Clientに'404 Not Found'をお知らせする。
+ * 
+ * ************************************************/
+
+void not_found(int client)
+{
+    char buf[1024];
+
+    sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, SERVER_STRING);
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<BODY><P>The server could not fulfill\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "your request because the resource specified\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "is unavailable or nonexistent.\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "</BODY></HTML>\r\n");
+    send(client, buf, strlen(buf), 0);
+}
+
+/**************************************************
+ * 
+ * 
+ * 
+ * 
+ * ************************************************/
+
+void headers(int client, const char *filename)
+{
+    char buf[1024];
+    (void)filename;
+
+    strcpy(buf, "HTTP/1.0 200 OK\r\n");
+    send(client, buf, strlen(buf), 0);
+    strcpy(buf, SERVER_STRING);
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    strcpy(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+}
+
+/**************************************************
+ * 
+ * 
+ * 
+ * 
+ * ************************************************/
+
+void serve_file(int client, const char *filename)
+{
+    FILE *resource = NULL;
+    int numchars = 1;
+    char buf[1024];
+
+    buf[0] = 'A'; buf[1] = '\0';
+    while((numchars > 0) && strcmp("\n", buf))
+        numchars = get_line(client, buf, sizeof(buf));
+
+    resource = fopen(filename, "r");
+    if(resource == NULL)
+        not_found(client);
+    else 
+    {
+        headers(client, filename);
+        cat(client, resource);
+    }
+    fclose(resource);
+}
+
+
+/**************************************************
+ * 
+ * 
+ * 
+ * 
+ * ************************************************/
+
 
 void accept_request(int client)
 {
-    char buf[1024];
+    char buf[1024];//バファのサイズを定義する（最大1024バイト）
     int numchars;
     char method[255];
     char url[255];
@@ -53,7 +176,7 @@ void accept_request(int client)
     int cgi = 0;
     char *query_string = NULL;
     numchars = get_line(client, buf, sizeof(buf));
-    // C標準ライブラリ(#include<stdio.h>)のgets(char *buff) 及び fgets(char * buf, int buffersize, *fstream)と似てる;
+    //後には定義される。
     i = 0; j = 0;
     while(!ISspace(buf[j]) && (i < sizeof(method) - 1))
     {
@@ -87,9 +210,125 @@ void accept_request(int client)
 
     if(strcasecmp(method, "GET") == 0)
     {
+        query_string = url;
+
+        while((*query_string != '?') && (*query_string != '\0'))
+            query_string++;
+        
+        if(*query_string == '?')
+        {
+
+            cgi = 1;
+
+            *query_string = '\0';
+
+            query_string++;
+        }
+
 
     }
 
+    sprintf(path, "htdocs%s", url);
 
+    if(path[strlen(path) - 1] == '/')
+        strcat(path, "index.html");
+
+    if(stat(path, &st) == -1)
+    {
+        while((numchars > 0) && strcmp("\n", buf))
+            numchars = get_line(client, buf, sizeof(buf));
+
+        not_found(client);
+    }
+    else 
+    {
+        if((st.st_mode & S_IFMT) == S_IFDIR)
+            strcat(path, "/index.html");
+
+        if( (st.st_mode & S_IXUSR) ||
+            (st.st_mode & S_IXGRP) ||
+            (st.st_mode & S_IXOTH)    )
+            cgi = 1;
+        
+        if(!cgi)
+        {
+            serve_file(client, path);
+        }
+        else
+        {
+            execute_cgi(client, path, method, query_string);
+        }
+       
+    }
+    close(client);
+}
+
+/**************************************************
+ * 
+ * 
+ * 
+ * 
+ * ************************************************/
+
+void bad_request(int client)
+{
+    char buf[1024];
+
+    sprintf(buf, "HTTP/1.0 400 BAD REQUEST\r\n");
+    send(client, buf, sizeof(buf), 0);//ssize_t send(int, const void*, size_t, int)は、#include<sys/socket.h>に定義される。
+    sprintf(buf, "Content-type: text/html\r\n");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "<P>Your browser sent a bad request, ");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "such as a POST without a Content-Length.\r\n");
+    send(client, buf, sizeof(buf), 0);
 
 }
+
+/**************************************************
+ * 
+ * 
+ * 
+ * 
+ * ************************************************/
+
+void cat(int client, FILE *resource)
+{
+    char buf[1024];
+    fgets(buf, sizeof(buf), resource);
+    while(!feof(resource))
+    {
+        send(client, buf, strlen(buf), 0);
+        fgets(buf, sizeof(buf), resource);
+    }
+}
+
+/**************************************************
+ * 
+ * 
+ * 
+ * 
+ * ************************************************/
+
+void cannot_execute(int client)
+{
+    char buf[1024];
+    sprintf(buf, "HTTP/1.0 500 Internal Server Error\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<P>Error prohibited CGI execution.\r\n");
+    send(client, buf, strlen(buf), 0);
+}
+
+/**************************************************
+ * 
+ * 
+ * 
+ * 
+ * ************************************************/
+
